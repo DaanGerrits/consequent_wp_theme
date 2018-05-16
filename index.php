543a699faf44f8cac91d477d7a73e3ff193459a7
Daan <info@daangerrits.nl>
2018-05-16 23:55:16 +0200

    changed layout en styles for basic pages in index.php

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php get_header(); ?>
 
-<main id="content" class="container">
-  <?php while( have_posts() ) : the_post() ?>
-    <article class="col-md-8">
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-    </article>
-  <?php endwhile; ?>
+<div id="main-content" class="container not-front">
+  <div class="row">
+    <main id="content" class="col-md-7">
+      <?php while( have_posts() ) : the_post() ?>
+        <article>
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+        </article>
+      <?php endwhile; ?>
+    </main>
 
-  <aside class="sidebar col-md-4">
-    <?php get_sidebar(); ?>
-  </aside>
-</main>
+    <aside class="sidebar col-md-4 col-md-offset-1">
+      <?php get_sidebar(); ?>
+    </aside>
+  </div>
+</div>
+
 
 <?php get_footer(); ?>
